6114cbe81dfd7a7a2dde6cdaef3349314ef88192
kaifransson <franssonkai@gmail.com>
2017-08-19 00:20:04 +0200

    dynamically assign port

--- src/Main.hs
+++ src/Main.hs
@@ -5,8 +5,11 @@ module Main where
 import           Network.HTTP.Types
 import           Network.Wai
 import           Network.Wai.Handler.Warp
+import           System.Environment
 import           Web.Simple.Controller
 
 main :: IO ()
-main = run 8000 $ controllerApp () $ do
-  routeTop . respond $ responseLBS status200 [(hContentType, "text/plain")] "Hello world!"
+main = do
+  port <- maybe 8000 read <$> lookupEnv "PORT"
+  run port $ controllerApp () $ do
+    routeTop . respond $ responseLBS status200 [(hContentType, "text/plain")] "Hello world!"
