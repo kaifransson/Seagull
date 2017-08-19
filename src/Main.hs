abe290d1519259273c5cdd335a2d9c0640245bf1
kaifransson <franssonkai@gmail.com>
2017-08-19 11:38:19 +0200

    blaze it (with style)

--- src/Main.hs
+++ src/Main.hs
@@ -2,14 +2,26 @@
 
 module Main where
 
+import           Data.String
 import           Network.HTTP.Types
 import           Network.Wai
 import           Network.Wai.Handler.Warp
 import           System.Environment
+import           Text.Blaze.Html.Renderer.Pretty
+import qualified Text.Blaze.Html5                as H
+import qualified Text.Blaze.Html5.Attributes     as A
 import           Web.Simple.Controller
+
+index :: H.Html
+index = H.docTypeHtml $ do
+  H.head $ do
+    H.title "Seagull"
+  H.body $ do
+    H.h1 "Hello world!"
+    H.p "Hello, friend"
 
 main :: IO ()
 main = do
   port <- maybe 8000 read <$> lookupEnv "PORT"
   run port $ controllerApp () $ do
-    routeTop . respond $ responseLBS status200 [(hContentType, "text/plain")] "Hello world!"
+    routeTop . respond $ responseLBS status200 [(hContentType, "text/html")] (fromString . renderHtml $ index)
