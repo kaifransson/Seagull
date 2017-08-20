c5a940b406906144825e016562226700fc943e2a
kaifransson <franssonkai@gmail.com>
2017-08-20 12:59:33 +0200

    trim unnecessary do:s

--- src/Seagull/Content.hs
+++ src/Seagull/Content.hs
@@ -8,12 +8,12 @@ import qualified Text.Blaze.Html5.Attributes as A
 
 index :: H.Html
 index = H.docTypeHtml $ do
-  H.head $ do
+  H.head $
     H.title "Seagull"
   H.body $ do
     H.h1 "Seagull"
     H.h3 "aka MåS aka MaaS aka Meds as a Service"
-    H.form H.! A.method "POST" $ do
+    H.form H.! A.method "POST" $
       H.button "Show"
-    H.footer $ do
+    H.footer $
         H.span "this is webscale"
