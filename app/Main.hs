6f6e0e753a179d09c926c390b867f332ac0d9646
kaifransson <franssonkai@gmail.com>
2017-08-22 10:08:11 +0200

    add auth middleware skeleton

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,18 @@
 module Main where
 
+import           Network.Wai
 import           Network.Wai.Handler.Warp
 import           System.Environment
 import           Web.Simple.Controller
 
 import           Seagull
 
+authMiddleware :: Middleware
+authMiddleware = id
+
 main :: IO ()
 main = do
   port <- maybe 8000 read <$> lookupEnv "PORT"
   let defaultState = SState False
-  run port $ controllerApp defaultState seagull
+  let app = controllerApp defaultState seagull
+  run port $ authMiddleware app
